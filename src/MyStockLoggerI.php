c03668ab06cd6e3dbec449ef0c8736d9bea2fd4b
Ján Forgáč <forgac@artfocus.cz>
2022-12-09 21:22:37 +0100

    MyStockLoggerI: parameter default value changed

--- src/MyStockLoggerI.php
+++ src/MyStockLoggerI.php
@@ -10,6 +10,6 @@ namespace MyStockWmsApiWrapper;
 interface MyStockLoggerI
 {
 
-	public function logg(string $message, array $context = []);
+	public function logg(string $message, array $context = null);
 
 }
